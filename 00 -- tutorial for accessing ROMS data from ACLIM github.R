85de5b7f5c3bb227110261747bc33cb1c744d9de
jennybigman <jennifersbigman@gmail.com>
2021-07-21 09:27:46 -0700

    organization

--- 00 -- tutorial for accessing ROMS data from ACLIM github.R
+++ 00 -- tutorial for accessing ROMS data from ACLIM github.R
@@ -1,10 +1,13 @@
 # accessing ROMS data from ACLIM github: https://github.com/kholsman/ACLIM2
 
+# first set working directory to ACLIM (current directory is my project on my machine)
+setwd("~/ACLIM2") # alternatively, could go right to ACLIM project
+
 # The below code will extract variables from the Level 2 and Level 3 netcdf files (.nc) and save them as 
 #compressed .Rdata files on your local Data/in/Newest/Rdata folder.
 
 #### Setup up the R worksace ####
-# First let’s get the workspace set up, will we step through an example downloading the hindcast and a 
+# First let’s get the workspace set up, we will then step through an example downloading the hindcast and a 
 # single projection (CMIP5 MIROC rcp8.5) but you can loop the code below to download the full set of 
 # CMIP5 projections.
 
@@ -85,7 +88,7 @@
     l2_info <- scan_l2(ds_list = dl,sim_list = "B10K-H16_CORECFS" )
     
     names(l2_info)
-    l2_info[["Bottom 5m"]]$vars
+    l2_info[["Bottom 5m"]]$vars 
     l2_info[["Surface 5m"]]$vars
     l2_info[["Integrated"]]$vars
     max(l2_info[["Integrated"]]$time_steps)
@@ -108,7 +111,7 @@
       'Integrated' = c("EupS","Cop","NCaS") ) 
    
     
-    # Let's sample the model years as close to Aug 1 as the model timesteps run:
+    # Let's sample the model years as close to Aug 1 as the model timesteps run: #### what is the timestep? ####
     # --> --> Tinker - try a different date
     tr          <- c("-08-1 12:00:00 GMT") 
     
@@ -120,6 +123,9 @@
       trIN        = tr,
       sub_varlist = svl,  
       sim_list    = sl  ) 
+    
+    
+    #### how do I work with these data I just downloaded #### ? 
     
 #### Data exploration with the minimal installation ####    
     
@@ -191,9 +197,158 @@ library(thredds)
    
    # for figure: Bottom temperature <2 degrees C (blue) and >=2 degrees C (green)
    
-#### Level 2 hindcasts ####
+#### Level 2 hindcasts #### Section 5.2
    
 # Level 2 data can be explored in the same way as the above indices but we will focus in the section below 
-# on a simple spatial plot and temporal index. The advantage of Level2 inidces is in the spatial resolution
+# on a simple spatial plot and temporal index. The advantage of Level2 indices is in the spatial resolution
 # and values outside of the survey area.
    
+# Section 5.2.1 Level 2 hindcasts: custom spatial indices
+   
+# create spatial plots of hindcast time periods for Aug 1 of each year
+   
+   # run this line if load_gis is set to F in R/setup.R:
+    source("R/sub_scripts/load_maps.R")  
+
+    # now create plots of average BT during four time periods
+    time_seg   <- list( '1970-1980' = c(1970:1980),
+                        '1980-1990' = c(1980:1990),
+                        '1990-2000' = c(1990:2000),
+                        '2000-2010' = c(2000:2010),
+                        '2010-2020' = c(2010:2020))
+    
+     # preview the datasets on the server:
+     tds_list_datasets(thredds_url = ACLIM_data_url)
+  
+    # assign the simulation to download
+    # --> Tinker: try selecting a different set of models to compare
+    sim        <- "B10K-K20_CORECFS" 
+    #ms <- c("B10K-H16_CORECFS","B10K-K20_CORECFS" )
+    
+    # Currently available Level 2 variables
+    dl     <- proj_l2_datasets$dataset  # datasets
+    
+    svl    <- list(
+                'Bottom 5m' = "temp",
+                'Surface 5m' = "temp",
+                'Integrated' = c("EupS","Cop","NCaS") )
+    
+    # Let's sample the model years as close to Aug 1 as the model timesteps run:
+    tr          <- c("-08-1 12:00:00 GMT") 
+    
+    # the full grid is large and takes a longtime to plot, so let's subsample the grid every 4 cells
+   
+    IDin       <- "_Aug1_subgrid"
+    var_use    <- "_bottom5m_temp"
+    
+    # open a "region" or strata specific nc file
+    fl         <- file.path(main,Rdata_path,sim,"Level2",
+                            paste0(sim,var_use,IDin,".Rdata"))
+    
+   # load data from level 2 nc files (approx <10sec)
+    startTime = Sys.time()
+    if(!file.exists(file.path(Rdata_path,fl))){
+      get_l2(
+        ID          = "_1990_subgrid",
+        overwrite   = T,
+        xi_rangeIN  = seq(1,182,10),
+        eta_rangeIN = seq(1,258,10),
+        ds_list     = dl[1],  # must be same length as sub_varlist
+        trIN        = tr,
+        yearsIN     = 1990,
+        sub_varlist = list('Bottom 5m' = "temp" ),  
+        sim_list    = sim  )
+    }
+    endTime  = Sys.time()
+    endTime  - startTime
+    
+    # load data from level 2 nc files for all years and vars (yearsIN = NULL by default)
+    #       NOTE: THIS IS SLOOOOOW..~ 2 min
+    startTime2 = Sys.time()
+    if(!file.exists(file.path(Rdata_path,fl))){
+      get_l2(
+        ID          = IDin,
+        overwrite   = T,
+        xi_rangeIN  = seq(1,182,10),
+        eta_rangeIN = seq(1,258,10),
+        ds_list     = dl,
+        trIN        = tr,
+        sub_varlist = svl,  
+        sim_list    = sim  )
+    }
+    endTime2  = Sys.time()
+    endTime2  - startTime2
+    
+    # load R data file
+    load(fl)   # temp
+    
+    # there are smarter ways to do this;looping because 
+    # we don't want to mess it up but this is slow...
+    i <-1
+    data_long <- data.frame(latitude = as.vector(temp$lat),
+                       longitude = as.vector(temp$lon),
+                       val = as.vector(temp$val[,,i]),
+                       time = temp$time[i],
+                       year = substr( temp$time[i],1,4),stringsAsFactors = F
+                       )
+    
+    for(i in 2:dim(temp$val)[3])
+      data_long <- rbind(data_long,
+                          data.frame(latitude = as.vector(temp$lat),
+                           longitude = as.vector(temp$lon),
+                           val = as.vector(temp$val[,,i]),
+                           time = temp$time[i],
+                           year = substr( temp$time[i],1,4),stringsAsFactors = F)
+                       )
+    
+    
+    # get the mean values for the time blocks from the rdata versions
+    # may throw "implicit NA" errors that can be ignored
+    tmp_var <-data_long # get mean var val for each time segment
+    j<-0
+    for(i in 1:length(time_seg)){
+      if(length( which(as.numeric(tmp_var$year)%in%time_seg[[i]] ))>0){
+        j <- j +1
+        mn_tmp_var <- tmp_var%>%
+          filter(year%in%time_seg[[i]],!is.na(val))%>%
+          group_by(latitude, longitude)%>%
+          summarise(mnval = mean(val,rm.na=T))
+        
+        mn_tmp_var$time_period <- factor(names(time_seg)[i],levels=names(time_seg))
+        
+      if(j == 1) mn_var <- mn_tmp_var
+      if(j >  1) mn_var <- rbind(mn_var,mn_tmp_var)
+       rm(mn_tmp_var)
+      }
+    }
+    
+    ## mnval is mean temp at the lat/long (for the time period) #### ?
+    
+    ## so this code already rotated the xi and eta axis to geographic axes? ####
+    
+    # convert results to a shapefile
+    L2_sf  <- convert2shp(mn_var%>%filter(!is.na(mnval)))
+    
+    p9_hind     <- plot_stations_basemap(sfIN = L2_sf,
+                                fillIN = "mnval",
+                                colorIN = "mnval",
+                                sizeIN=.6) +
+      #facet_wrap(.~time_period,nrow=2,ncol=3)+
+      facet_grid(.~time_period)+
+      scale_color_viridis_c()+
+      scale_fill_viridis_c()+
+      guides(
+        color =  guide_legend(title="Bottom T (degC)"),
+        fill  =  guide_legend(title="Bottom T (degC)")) +
+      ggtitle(paste(sim,var_use,IDin))
+   
+    # This is slow but it works (repeat dev.new() twice if in Rstudio)...
+    dev.new()
+    p9_hind
+    
+    if(update.figs)  
+      ggsave(file=file.path(main,"Figs/Hind_sub_grid_mn_BT_Aug1.jpg"),width=8,height=4)
+  
+    # graphics.off()
+   
+    #### START with 5.2.2 ####
