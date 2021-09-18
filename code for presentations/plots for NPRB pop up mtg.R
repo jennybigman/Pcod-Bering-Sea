0f4e93a0e5ff5b1865f9f01cb08c298e26b09c89
jennybigman <jennifersbigman@gmail.com>
2021-09-17 17:55:08 -0700

    organization

--- code for presentations/plots for NPRB pop up mtg.R
+++ code for presentations/plots for NPRB pop up mtg.R
@@ -131,4 +131,100 @@
 			 warm_v_cold_hs,
 			 width = 10, height = 7, units = "in")
 		
+  	
+  	## with color blocks
+  	
+  	# maps for cold vs warm years
+
+	hs_2008_plot_noleg <- 
+		ggplot() +
+  	geom_sf(data = temp_2008_sf, aes(color = sp_hab_suit)) +
+  	geom_sf(data = world_map_data, fill = "grey", lwd = 0) +
+		coord_sf(crs = 3338) +
+ 		scale_x_continuous(
+ 			breaks = c(-175, -170, -165, -160),
+ 			labels = c("-175˚", "-170˚", "-165˚", "-160˚"),
+ 			name = "Longitude",
+ 			limits = c(-1400000, -150000)
+ 		) +
+ 	  scale_y_continuous(
+ 			breaks = c(55, 60),
+ 			limits = c(470000, 1900000),
+ 			name = "Latitude",
+ 		) +
+		scale_color_gradientn(colors = c("#B3E5FC", "#B3E5FC", 
+																			"#01579B", "#01579B",
+																			"#00345C", "#00345C"),
+												  values = c(0, 0.499, 0.5, 0.899, 0.9, 1),
+												  breaks = c(0.1, 0.5, 0.9),
+												  labels = format(c(0.1, 0.5, 0.9)),
+												  limits = c(0, 1)) +
+    labs(colour = "Spawning\nhabitat\nsuitability") +
+		theme_bw() +
+ 		theme(
+ 			legend.position = "none",
+  		strip.background = element_rect(colour="white", fill="white"),
+    	panel.border = element_rect(colour = "black"),
+  		strip.text = element_text(size = 14, face = "bold"),
+  		axis.text = element_text(size = 10),	
+  		axis.title = element_text(size = 14),
+  		panel.spacing.x=unit(0, "lines"),
+  		legend.title.align=0.5)
   		
+	hs_2016_plot_noy <- 
+		ggplot() +
+  	geom_sf(data = temp_2016_sf, aes(color = sp_hab_suit)) +
+  	geom_sf(data = world_map_data, fill = "grey", lwd = 0) +
+		coord_sf(crs = 3338) +
+ 		scale_x_continuous(
+ 			breaks = c(-175, -170, -165, -160),
+ 			labels = c("-175˚", "-170˚", "-165˚", "-160˚"),
+ 			name = "Longitude",
+ 			limits = c(-1400000, -150000)
+ 		) +
+ 	  scale_y_continuous(
+ 			breaks = c(55, 60),
+ 			limits = c(470000, 1900000),
+ 			name = "Latitude",
+ 		) +
+		scale_color_gradientn(colors = c("#B3E5FC", "#B3E5FC", 
+																			"#01579B", "#01579B",
+																			"#00345C", "#00345C"),
+												  values = c(0, 0.499, 0.5, 0.899, 0.9, 1),
+												  breaks = c(0.1, 0.5, 0.9),
+												  labels = format(c(0.1, 0.5, 0.9)),
+												  limits = c(0, 1)) +
+    labs(colour = "Spawning\nhabitat\nsuitability") +
+		theme_bw() +
+ 		theme(
+			legend.title = element_text(size = 12),
+  		legend.text = element_text(size = 12),  	
+			strip.background = element_rect(colour="white", fill="white"),
+    	panel.border = element_rect(colour = "black"),
+  		strip.text = element_text(size = 14, face = "bold"),
+			axis.text.x = element_text(size = 10),		
+  		axis.text.y = element_blank(),
+    	axis.ticks.y = element_blank(),
+    	axis.title.y = element_blank(),
+  		axis.title.x = element_text(size = 14),
+  		panel.spacing.x=unit(0, "lines"),
+  		legend.title.align=0.5)
+  
+
+   plot1 <- hs_2008_plot_noleg + 
+   	        theme(plot.margin = unit(c(0.2, 0, 0.2, 0.2), "in")) +
+   	        ggtitle("2008: Cold Year") +
+   	        theme(plot.title = element_text(size = 14, hjust = 0.5,  face = "bold"))
+   
+   plot2 <- hs_2016_plot_noy + 
+   	        theme(plot.margin = unit(c(0.2, 0.2, 0.2, -0.05), "in")) +
+   	        ggtitle("2016: Warm Year") +
+   	        theme(plot.title = element_text(size = 14, hjust = 0.5,  face = "bold"))
+
+	 warm_v_cold_hs <- plot1 + plot2 
+	 
+  	ggsave("./output/plots/warm_v_cold_hs_colorblocks.png",
+			 warm_v_cold_hs,
+			 width = 10, height = 7, units = "in")
+		
+	
