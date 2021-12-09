f1b1e02ecbdefe79b229e9758aeaa25283963552
jennybigman <jennifersbigman@gmail.com>
2021-12-08 17:23:31 -0800

    reordering files and adding in projections

--- scripts/01 - load each time.R
+++ scripts/01 - load each time.R
@@ -1,4 +1,4 @@
-	# load each time #### TEST
+	# load each time 
 
 	#### libraries ####
 	library(here)
@@ -24,17 +24,39 @@
 	symbol<-"\u2265"
 
 	#### load and transform data ####
-	ROMS_hindcast_dat  <- fread(file = "./data/ROMS_hindcast_dat.csv")
 	
+	# hindcast data
+	ROMS_hindcast_dat  <- fread(file = "./data/ROMS_hindcast_dat.csv") %>% filter(., year != 2021)
+	
+	# projected data
+	years_drop <- c(2021:2099)
+	
+	ROMS_projected_dat <- fread(file = "./data/ROMS_projected_dat.csv") %>%
+		filter(., year %in% years_drop) %>%
+		mutate(latitude = Lat,
+					 longitude = Lon)
+
 	
 	# reorder for plotting
 	ROMS_hindcast_dat$month_name <- factor(ROMS_hindcast_dat$month_name)
   ROMS_hindcast_dat$month_name <- fct_reorder(ROMS_hindcast_dat$month_name, 
   																		ROMS_hindcast_dat$month)
+  
+  ROMS_projected_dat$month_name <- factor(ROMS_projected_dat$month_name)
+  ROMS_projected_dat$month_name <- fct_reorder(ROMS_projected_dat$month_name, 
+  																		ROMS_projected_dat$month)
+
 
   # convert to a shapefile
   ROMS_hindcast_dat_sf <- ROMS_hindcast_dat %>%
   	st_as_sf(coords = c("long_not_360", "latitude"), crs = 4326)
+  
+  ROMS_projected_dat_sf <- ROMS_projected_dat %>%
+  		mutate(long_not_360 = case_when(
+				longitude >= 180 ~ longitude - 360,
+				longitude < 180 ~ longitude)) %>%
+  	st_as_sf(coords = c("long_not_360", "latitude"), crs = 4326)
+	
 	
 	#### load map ####
 	world_map_data <- ne_countries(scale = "medium", returnclass = "sf") 
