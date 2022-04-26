15d8516038981dfa5726493775966f23bfce8b30
jennybigman <jennifersbigman@gmail.com>
2022-04-26 16:40:47 -0700

    amend plot

--- scripts/04 - mean latitude.R
+++ scripts/04 - mean latitude.R
@@ -537,3 +537,180 @@
 #  		
 #
 #	#
+  
+  #### mapping ####
+  
+  ## hindcast 
+  
+ 	coords_hind_func <- function(x){
+		
+		new_dat <- ROMS_hindcast_dat %>%
+			filter(., sp_hab_suit >= x)
+		
+		new_dat_sum <- new_dat %>%
+			group_by(year) %>%
+			summarise(latitude = mean(latitude),
+								longitude = mean(longitude)) %>%
+			mutate(sp_hab_threshold = x)
+
+		new_dat_sum		
+	}
+	
+	sp_hab_thresholds <- c(0.5, 0.9)
+	
+	coords_hind <- lapply(sp_hab_thresholds, coords_hind_func)
+	
+	coords_hind <- bind_rows(coords_hind) 
+	
+	coords_hind_sf <- coords_hind %>%
+		mutate(long_not_360 = case_when(
+				longitude >= 180 ~ longitude - 360,
+				longitude < 180 ~ longitude)) %>%
+  	st_as_sf(coords = c("long_not_360", "latitude"), crs = 4326, remove = FALSE)
+
+	
+	## projection
+	
+	coords_proj_func <- function(x){
+		
+		new_dat <- ROMS_projected_dat %>%
+			filter(., sp_hab_suit_var >= x)
+		
+		new_dat_sum <- new_dat %>%
+			group_by(projection, year) %>%
+			summarise(proj_mean_lat = mean(latitude),
+								proj_mean_long = mean(long_not_360))  %>%
+			mutate(sp_hab_threshold = x)
+
+		new_dat_sum		
+	}
+	
+	sp_hab_thresholds <- c(0.5, 0.9)
+	
+	coords_proj <- lapply(sp_hab_thresholds, coords_proj_func)
+	
+	coords_proj <- bind_rows(coords_proj) 
+	
+	years_proj <- 2020:2099
+	
+	coords_proj <- coords_proj %>%
+		filter(year %in% years_proj)
+	
+	coords_proj_sf <- coords_proj %>%
+  	st_as_sf(coords = c("proj_mean_long", "proj_mean_lat"), crs = 4326, remove = FALSE)
+		
+	
+	# map
+
+	map <- 
+		 	ggplot(coords_hind_sf) +
+			geom_sf(data = world_map_data, fill = "grey", lwd = 0) +
+			coord_sf(crs = 3338) +
+ 			scale_x_continuous(
+ 				breaks = c(-175, -170, -165, -160),
+ 				labels = c("-175˚", "-170˚", "-165˚", "-160˚"),
+ 				name = "Longitude",
+ 				limits = c(-1400000, -150000)) +
+ 			scale_y_continuous(
+ 				breaks = c(55, 60),
+ 				limits = c(470000, 1900000),
+ 				name = "Latitude") +
+  		geom_point(aes(color = year, alpha = 0.5,
+  			geometry = geometry),
+    		stat = "sf_coordinates") +
+		scale_color_viridis_c()
+	
+	ggsave("./output/plots/map_yrcentroids_05.png",
+			 map_yrcentroids_05,
+			 width = 10, height = 7, units = "in")
+
+	#### keep playing
+	
+	
+		map <- 
+		 	ggplot() +
+			geom_sf(data = world_map_data, fill = "grey", lwd = 0) +
+			coord_sf(crs = 3338) +
+ 			scale_x_continuous(
+ 				breaks = c(-175, -170, -165, -160),
+ 				labels = c("-175˚", "-170˚", "-165˚", "-160˚"),
+ 				name = "Longitude",
+ 				limits = c(-1400000, -150000)) +
+ 			scale_y_continuous(
+ 				breaks = c(55, 60),
+ 				limits = c(470000, 1900000),
+ 				name = "Latitude") +
+  		geom_point(data = coords_hind_sf, aes(color = year, alpha = 0.5,
+  			geometry = geometry),
+    		stat = "sf_coordinates") +
+			geom_point(data = coords_proj_sf, aes(color = year, alpha = 0.5,
+  			geometry = geometry),
+    		stat = "sf_coordinates") +
+		scale_color_viridis_c()
+		
+		
+	coords_proj_sf$scen <- NA
+	
+	coords_proj_sf$scen[coords_proj_sf$projection == "ssp126"] <- "low emission\n(ssp126)"
+	coords_proj_sf$scen[coords_proj_sf$projection == "ssp585"] <- "high emission\n(ssp585)"
+
+	coords_proj_sf$scen_f = factor(coords_proj_sf$scen, 
+																		levels=c('low emission\n(ssp126)', 'high emission\n(ssp585)'))
+	
+
+
+	coords_proj_sf$thresh <- NA
+	coords_hind_sf$thresh <- NA
+
+	coords_proj_sf$thresh[coords_proj_sf$sp_hab_threshold == 0.5] <- "potential"
+	coords_proj_sf$thresh[coords_proj_sf$sp_hab_threshold == 0.9] <- "core"
+	
+	coords_hind_sf$thresh[coords_hind_sf$sp_hab_threshold == 0.5] <- "potential"
+	coords_hind_sf$thresh[coords_hind_sf$sp_hab_threshold == 0.9] <- "core"
+	
+	
+	coords_proj_sf$scen_f = factor(coords_proj_sf$scen, 
+																		levels=c('low emission\n(ssp126)', 'high emission\n(ssp585)'))
+
+	
+		map_mean_lats <- 
+		 	ggplot() +
+			geom_sf(data = world_map_data, fill = "grey", lwd = 0) +
+			coord_sf(crs = 3338) +
+ 			scale_x_continuous(
+ 				breaks = c(-175, -170, -165, -160),
+ 				labels = c("-175˚", "-170˚", "-165˚", "-160˚"),
+ 				name = "Longitude",
+ 				limits = c(-1400000, 10000)) +
+ 			scale_y_continuous(
+ 				breaks = c(55, 60),
+ 				limits = c(470000, 1900000),
+ 				name = "Latitude") +
+  		geom_point(data = coords_hind_sf, aes(color = year, 
+  			geometry = geometry), alpha = 0.5, 
+    		stat = "sf_coordinates") +
+			geom_point(data = coords_proj_sf, aes(color = year, 
+  			geometry = geometry), alpha = 0.5, 
+    		stat = "sf_coordinates") +
+		facet_grid(thresh ~ scen_f) +
+		scale_color_viridis_c(
+			breaks = c(1980, 2020, 2060, 2099)
+		) +
+	  theme_bw() +
+ 		theme(
+			legend.title.align = 0.5,
+			#legend.position = c(0.89, 0.72),
+			#legend.background = element_blank(),
+			#legend.title = element_text(size = 9),
+			#legend.text = element_text(size = 8),
+ 			panel.border = element_rect(color = "#666666"),
+ 			strip.text.x = element_text(size = 12, face = "bold",  color = "black"),
+ 			strip.text.y = element_text(size = 12, face = "bold",  color = "black"),
+ 			strip.background = element_blank(),
+ 			axis.text = element_text(size = 10,  color = "#666666"),	
+  		axis.title = element_text(size = 12,  color = "#666666"),
+ 			axis.ticks = element_line( color = "#666666"))
+
+		ggsave("./output/plots/mean_lats_map.png",
+			 map_mean_lats,
+			 width = 10, height = 5, units = "in")
