246e731b0c3d3fafe34a8361ec89747cd49b5738
jennybigman <jennifersbigman@gmail.com>
2022-04-26 16:41:33 -0700

    ensure each grid cell only plotted once

--- scripts/10 - mapping larval distribution.R
+++ scripts/10 - mapping larval distribution.R
@@ -7,8 +7,11 @@
 	catch0_dat <- read_csv("./data/PcodLarvalCatchwZeros_BS_60Bon_Feb2022.csv")
 	
 	catch0_dat$GMT_DATE_TIME <- as.POSIXct(catch0_dat$GMT_DATE_TIME)
+	
+	catch0_dat_trim <- catch0_dat %>%
+		filter(LARVALCATCHPER10M2 == 0) %>%
+		distinct(across(c(LAT, LON)), .keep_all = TRUE)
 		
-	length_dat$MONTH <- month(length_dat$GMT_DATE_TIME)
 	
 	# convert longs and rename cols
 	length_dat_sf <- length_dat %>%
@@ -18,56 +21,56 @@
 				LON < 180 ~ LON)) %>%
 		st_as_sf(coords = c("LON_not_360", "LAT"), crs = 4326, remove = FALSE)
 
-	catch0_dat_sf <- catch0_dat %>%
+	catch0_dat_trim_sf <- catch0_dat_trim %>%
 		mutate(LON_not_360 = case_when(
 				LON >= 180 ~ LON - 360,
 				LON < 180 ~ LON)) %>%
 		st_as_sf(coords = c("LON_not_360", "LAT"), crs = 4326, remove = FALSE)
 
-	# map density
-	breaks_x <- c(-175, -170, -165, -160)
-	labels_x <- c("-175˚", "-170˚", "-165˚", "-160˚") 
-	limits_x <- c(-1400000, -150000)
-	
-	breaks_y <- c(55, 60)
-	limits_y <- c(470000, 1900000)
-
 	# larval plot year ####
+
 	larval_yr_plot <- 
-  	ggplot(catch0_dat_sf) +
+  	ggplot() +
+		geom_sf(data = catch0_dat_trim_sf,
+						aes(color = LARVALCATCHPER10M2),
+						color = "black", shape = 4, alpha = 0.5, size = 1) +
 		geom_sf(data = length_dat_sf, 
 						aes(color = CORRECTED_LENGTH, 
 								size = LARVALCATCHPER10M2),
 						alpha = 0.5) +
-		geom_sf(aes(color = LARVALCATCHPER10M2),
-			data = . %>% filter(LARVALCATCHPER10M2 == 0), 
-			color = "black", shape = 4, alpha = 0.5) +
+		scale_color_viridis_c() +
 		geom_sf(data = world_map_data, fill = "grey", lwd = 0) +
 		coord_sf(crs = 3338) +
-	  facet_wrap(~ YEAR, nrow = 4) +
- 					scale_x_continuous(
- 						breaks = breaks_x,
- 						labels =labels_x,
- 						name = "Longitude",
- 						limits = limits_x
- 					) +
- 					scale_y_continuous(
- 						breaks = breaks_y,
- 						limits = limits_y,
- 						name = "Latitude",
- 					) +
-					theme_bw() +
- 					theme(
- 						strip.text = element_text(size = 12, face = "bold"),
- 						strip.background = element_blank(),
- 						axis.text = element_text(size = 10),	
-  					axis.title = element_text(size = 12),
-  					legend.title.align=0.5)
+ 		scale_x_continuous(
+ 			breaks = breaks_x,
+ 			labels =labels_x,
+ 			name = "Longitude",
+ 			limits = limits_x) +
+ 		scale_y_continuous(
+ 			breaks = breaks_y,
+ 			limits = limits_y,
+ 			name = "Latitude") +
+		labs(color = "length (mm)", size = expression(paste("catch (per 10m"^{2}*")"))) +
+		theme_bw() +
+		theme(
+			legend.title = element_text(size = 8),
+  		legend.title.align=0.5,
+  		legend.text = element_text(size = 6),
+ 			axis.text = element_text(size = 8, colour = "grey50"),
+  	  axis.ticks = element_line(colour = "grey50"),
+  	  axis.line = element_line(colour = "grey50"),
+			axis.title = element_text(size = 10, color = "grey50"),
+  	  panel.border = element_rect(fill = NA, color = "grey50"))
 	
 		ggsave("./output/plots/larval_yr_plot.png",
 			 larval_yr_plot,
-			 width = 20, height = 10, units = "in")
+			 width = 5, height = 5, units = "in")
 
+
+		
+		
+		
+		
 	# larval plot year + month
 	
 	larval_mo_plot <- 
